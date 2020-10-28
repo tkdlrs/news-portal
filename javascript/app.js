06035446a9556f9abf4abd9f610248adb9ea42cf
unknown <tkd.lsanchez@gmail.com>
2020-10-28 12:00:37 -0600

    added the functionality to the users feed page and viewing an individual story

--- javascript/app.js
+++ javascript/app.js
@@ -13,11 +13,16 @@ class topNavigationMenu extends HTMLElement {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link text-white" href="/">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Link</a>
+                    <a class="nav-link text-white" href="./add-story.html">Add Story</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="./view-all-stories.html">View All Stories</a>
+                </li>
+
+                <!--
                 <li class="nav-item dropdown">
                     <a class="nav-link text-white dropdown-toggle" href="#" id="navbarDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -30,6 +35,7 @@ class topNavigationMenu extends HTMLElement {
                         <a class="dropdown-item" href="#">Something else here</a>
                     </div>
                 </li>
+                -->
             </ul>
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
@@ -42,53 +48,75 @@ class topNavigationMenu extends HTMLElement {
 };
 
 function queryDBAndSetUpUserStoriesFeed(appender) {
-    if(appender != null) {
+    if (appender != null) {
         // This will need to be updated to match your backend. I'm going to fetch my fake json data to similate an async request to the database
         fetch('./javascript/fake-data.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
+            .then(response => response.json())
+            .then(docs => {
+                docs.forEach(doc => {
+                    renderStoryInFeed(doc, appender);
+                })
+            })
+            .catch(err => console.error(err));
     };
+};
+
+function queryDBAndSetUpNewsStory(appender, urlParameter) {
+    if (appender !== null) {
+        fetch('./javascript/fake-data.json')
+            .then(response => response.json())
+            .then(docs => {
+                docs.forEach(doc => {
+                    const regex = /\s/gi;
+                    const cleanTitle = doc.title.trim().replace(regex, '-').toLowerCase();
+                    if (cleanTitle === urlParameter) {
+                        renderNewsStory(doc, appender);
+                    };
+                })
+            })
+            .catch(err => console.error(err));
+    }
 };
 
 // EVENTS 
 // Event Delegation for dynamic elements in the forms
 function setupEventDelegationForNews(element) {
-    if(element !== null) {
+    if (element !== null) {
         element.addEventListener("click", (e) => {
             // Adding paragraph 
-            if(e.target && e.target.matches("#add-paragraph")) {
+            if (e.target && e.target.matches("#add-paragraph")) {
                 e.stopPropagation();
                 renderAdditionalParagraph(document.querySelector('#story-paragraphs'))
             };
             // Removing paragraph
-            if(e.target && e.target.matches(".remove-paragraph")) {
+            if (e.target && e.target.matches(".remove-paragraph")) {
                 e.stopPropagation();
                 e.target.closest('.paragraph').remove();
                 const paragraphs = document.querySelectorAll('.paragraph');
-                for(let i = 0; i < paragraphs.length; i++) {
-                    paragraphs[i].querySelector('label').setAttribute('for', `paragraph-${(i+1)}`);
-                    paragraphs[i].querySelector('label').innerText = `Paragraph ${(i+1)}`;
-                    paragraphs[i].querySelector('textarea').setAttribute('id', `paragraph-${(i+1)}`);
+                for (let i = 0; i < paragraphs.length; i++) {
+                    paragraphs[i].querySelector('label').setAttribute('for', `paragraph-${(i + 1)}`);
+                    paragraphs[i].querySelector('label').innerText = `Paragraph ${(i + 1)}`;
+                    paragraphs[i].querySelector('textarea').setAttribute('id', `paragraph-${(i + 1)}`);
                 }
             };
             // Adding art 
-            if(e.target && e.target.matches('#add-art')) {
+            if (e.target && e.target.matches('#add-art')) {
                 e.stopPropagation();
                 renderAdditionalArt(document.querySelector('#story-art'));
             };
             // Removing art
-            if(e.target && e.target.matches('.remove-art')) {
+            if (e.target && e.target.matches('.remove-art')) {
                 e.stopPropagation();
                 e.target.closest('.art').remove();
                 const arts = document.querySelectorAll('.art');
-                for(let i = 0; i < arts.length; i++) {
-                    arts[i].querySelector('label.image').setAttribute('for', `image-${(i+1)}`);
-                    arts[i].querySelector('label.image').innerText = `Image ${(i+1)}`;
-                    arts[i].querySelector('input.image').setAttribute('id', `image-${(i+1)}`);
+                for (let i = 0; i < arts.length; i++) {
+                    arts[i].querySelector('label.image').setAttribute('for', `image-${(i + 1)}`);
+                    arts[i].querySelector('label.image').innerText = `Image ${(i + 1)}`;
+                    arts[i].querySelector('input.image').setAttribute('id', `image-${(i + 1)}`);
 
-                    arts[i].querySelector('label.caption').setAttribute('for', `caption-${(i+1)}`);
-                    arts[i].querySelector('label.caption').innerText = `Caption ${(i+1)}`;
-                    arts[i].querySelector('input.caption').setAttribute('id', `caption-${(i+1)}`);
+                    arts[i].querySelector('label.caption').setAttribute('for', `caption-${(i + 1)}`);
+                    arts[i].querySelector('label.caption').innerText = `Caption ${(i + 1)}`;
+                    arts[i].querySelector('input.caption').setAttribute('id', `caption-${(i + 1)}`);
                 }
             }
         })
@@ -97,7 +125,7 @@ function setupEventDelegationForNews(element) {
 
 // News item Submission
 function submitNewNewsItem(formElement) {
-    if(formElement !== null) {
+    if (formElement !== null) {
         formElement.addEventListener('submit', (e) => {
             e.preventDefault();
             let tags = [];
@@ -113,7 +141,7 @@ function submitNewNewsItem(formElement) {
             // figure out tags array
             const allPotentialTags = document.querySelectorAll('.tags[type="checkbox"]');
             allPotentialTags.forEach(tag => {
-                if(tag.checked){
+                if (tag.checked) {
                     tags.push(tag.value);
                 }
             });
@@ -129,7 +157,7 @@ function submitNewNewsItem(formElement) {
             // figure out all of the art that the story has
             const allArts = document.querySelectorAll('.art');
             allArts.forEach(art => {
-                arts.push({image: art.querySelector('input.image').value, caption: art.querySelector('input.caption').value})
+                arts.push({ image: art.querySelector('input.image').value, caption: art.querySelector('input.caption').value })
             });
             newsItem.arts = arts;
 
@@ -187,14 +215,95 @@ function renderAdditionalArt(appender) {
     appender.innerHTML += htmlTemplate;
 };
 
-// story feed template
+// stories feed template
 function renderStoryInFeed(data, appender) {
+    const regex = /\s/gi;
+    const cleanTitle = data.title.trim().replace(regex, '-').toLowerCase();
     const htmlTemplate = `
-
+    <div class="card border-2 card-news mb-3">
+    <div class="row">
+       <div class="col-md-4">
+            <a class="" href="./view-story.html?${cleanTitle}" style="background-image: url('${data.thumbnailImage}');">
+                <img class="w-100" src="${data.thumbnailImage}" alt="${data.title}">
+                <span class="sr-only">Read Story</span>
+            </a>
+        </div>
+       <div class="col-md-8">
+        <div class="card-header bg-transparent">
+            <h2 class="card-title h4 mt-2"><a class="text-blue" href="./view-story.html?${cleanTitle}">${data.title}</a></h2>
+        </div>
+        <div class="card-body">
+            <p class="card-text">${data.paragraphs[0]}</p>
+            <a href="./view-story.html?${cleanTitle}" class="card-link float-right">Read Story</a>
+            <div class="clearfix"></div>
+        </div>
+        <div class="card-footer bg-transparent">
+            <small class="float-right">${data.publishDate}</small>
+            <div class="clearfix"></div>
+        </div>
+       </div>
+    </div>
+ </div>
     `;
     appender.innerHTML += htmlTemplate;
-}
+};
 
+// individual story
+function renderNewsStory(data, appender) {
+    let combinedStoryAndArtTemplate = `
+    <p><strong>${data.publishDate}</strong> &mdash; ${data.paragraphs[0]}</p>
+    <div class="float-right m-1 bg-blue-light p-0 col-md-5">
+        <a href="${data.arts[0].image}">
+            <img class="w-100" src="${data.arts[0].image}" alt="${data.arts[0].caption}">
+        </a>
+        <p><em>${data.arts[0].caption}</em></p>
+	</div>
+    `;
+    let artsLength = data.arts.length;
+    let currentArt = 1;
+    for (let i = 1; i < data.paragraphs.length; i++) {
+        combinedStoryAndArtTemplate += `
+        <p>${data.paragraphs[i]}</p>
+        `;
+        console.log("(i - 1) % 5", (i - 1) % 5 === 0);
+
+        if (currentArt < artsLength && (i % 3) === 0) {
+            combinedStoryAndArtTemplate += `
+            <div class="${(currentArt % 2) ? 'float-left': 'float-right'} m-1 bg-blue-light p-0 col-md-5">
+                <a href="${data.arts[currentArt].image}">
+                    <img class="w-100" src="${data.arts[currentArt].image}" alt="${data.arts[currentArt].caption}">
+                </a>
+                <p><em>${data.arts[currentArt].caption}</em></p>						
+			</div>
+            `;
+            currentArt++;
+        }
+    };
+
+    const htmlTemplate = `
+<div class="row">
+    <div class="col-12">  
+        <h1>${data.title}</h1>
+        <div class="row justify-content-end">
+            <div class="col-sm-12 col-md-3">
+                <p class="h5">${data.publishDate}</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-12">
+        ${combinedStoryAndArtTemplate}
+    </div>
+    <div class="col-12">
+        <p class="text-center">###</p>
+        <p><span class="font-weight-bold">Writer(s)</span>: ${data.author}</p>
+    </div>
+</div>
+    `;
+
+    appender.innerHTML = htmlTemplate;
+};
 
 //
 window.addEventListener("DOMContentLoaded", () => {
@@ -206,4 +315,6 @@ window.addEventListener("DOMContentLoaded", () => {
     submitNewNewsItem(document.querySelector('#create-news-story-form'));
     // set up the news feed for regular users
     queryDBAndSetUpUserStoriesFeed(document.querySelector('#stories'));
+    // set up an individual news item
+    queryDBAndSetUpNewsStory(document.querySelector('#story'), document.URL.split('?')[1]);
 });
